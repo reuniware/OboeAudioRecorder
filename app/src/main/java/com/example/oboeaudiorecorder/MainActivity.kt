e310b821963058d98a1bbca6b6f0950648282e80
DidierV <vally@3ci.fr>
2020-06-28 12:12:16 +0200

    Permission stuff (RECORD_AUDIO permission)

--- app/src/main/java/com/example/oboeaudiorecorder/MainActivity.kt
+++ app/src/main/java/com/example/oboeaudiorecorder/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.oboeaudiorecorder
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +19,29 @@ class MainActivity : AppCompatActivity() {
         sample_text.text = stringFromJNI()
 
         buttonRecordAudio.setOnClickListener{
-            recordAudio()
+            val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                Log.i("", "Permission to record denied")
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), RECORD_REQUEST_CODE)
+            } else {
+                recordAudio()
+            }
+        }
+    }
+
+    val RECORD_REQUEST_CODE = 1234
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            RECORD_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i("", "Permission has been denied by user")
+                } else {
+                    Log.i("", "Permission has been granted by user")
+                    recordAudio()
+                }
+            }
         }
     }
 
